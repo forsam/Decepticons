0a90e9c5a8ce93e064f5453121cff9831b666df4
forsam <samfor-3@student.ltu.se>
2017-04-19 15:20:38 +0200

    made changes

--- theProgram/theProgram.ino
+++ theProgram/theProgram.ino
@@ -2,6 +2,7 @@
 
 // DEFINE ALL THE PINS //
 //-----------------------------------------------//
+  #define velocityPin 2
   #define steeringPin 3
   #define motorPin 4
   int lineSensorPins[] =  {5,6,7,8,9,10,11,12};
@@ -18,15 +19,29 @@
 
 // CREATE GLOBAL VARIABLES!! //
 //-----------------------------------------------//
-  /*for the speed monitoring*/
-  int RPM = 0;
-  unsigned long RPMTimeLastUpdate =0;
-
+  /*for the speed updates*/
+  float RPM[] = {0,0};
+  float RPMTimeLastUpdate = 0;
+  float RPMdt;
+  
   /*Distance monitoring*/
   long echoDuration, distanceToBrick;
 
   /*Linesensor monitoring*/
-  int lineSensorBool[] = {0,0,0,0,0,0,0,0}
+  int lineSensorBool[] = {0,0,0,0,0,0,0,0};
+
+  /*Essential controller stuff*/
+  float distanceTravelled = 0;
+  float acceleration = 0;
+  float velocity = 0;
+  float wantedVelocity = 5;
+  int inputSpeed = 102;
+  int Kp = 0.1;
+  
+  /*Basic variables*/
+  int mm = 0.001;
+  int wheelDiameter = 65*mm;
+  float RPMtoMS = wheelDiameter*3.1415/60;
 
 // CREATE FUNCTIONS //
 //-------------------------------------------------//
@@ -47,12 +62,35 @@
   }
 
   /*Updates the global variable rpm*/
-  int updateRpm()
+  void updateRpm()
   {
-      RPM = 60*0.25*1000 / ((millis() - RPMTimeLastUpdate));
+      Serial.print("detected");
+      RPMdt = (millis() - RPMTimeLastUpdate)/1000;
+      RPM[1] = RPM[0];
+      RPM[0] = 60*0.25*1000 / (RPMdt);
       RPMTimeLastUpdate = millis();
   }
+  void updateAcceleration()
+  {
+    acceleration = (RPM[1] - RPM[0])*RPMtoMS/RPMdt;
+  } 
 
+  void updateDistance()
+  {
+    distanceTravelled = distanceTravelled + RPM[0]*RPMtoMS*RPMdt;
+  }
+
+  void updateVelocity()
+  {
+    velocity = RPM[0]*RPMtoMS;
+  }
+
+  void updateInputSpeed()
+  {
+    double error = wantedVelocity - velocity;
+    inputSpeed = inputSpeed + error*Kp;
+  }
+  
   /*a function to check which linesensors that are high!*/
   void checkLineSensors()
   {
@@ -85,18 +123,27 @@
   void checkSensors()
   {
     checkLineSensors();
-    checkDistance();
+    //checkDistance();
+  }
+
+  void updateValues()
+  {
+    updateAcceleration();
+    updateDistance();
+    updateVelocity();
+    updateInputSpeed();
   }
 
   void execute()
   {
-
+    setSpeed(inputSpeed);
   }
 
 // RUN THE FIRST SETUP LOOP //
 //--------------------------------------------------------//
   void setup(void)
   {
+    Serial.begin(9600);
 
     attachInterrupt(0, magnetDetect, RISING);
 
@@ -113,6 +160,7 @@
     {
       pinMode(lineSensorPins[pin],INPUT);
     }
+    
     //Attach the echosensor!
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
@@ -124,5 +172,6 @@
   void loop(void)
   {
     checkSensors();
+    updateValues();
     execute();
   }
